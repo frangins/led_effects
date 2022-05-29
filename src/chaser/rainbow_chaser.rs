9a0046c4f6122fc4db22c58e2268bc3a46cfae9f
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-05-29 20:35:47 +0200

    chore: add license information

--- src/chaser/rainbow_chaser.rs
+++ src/chaser/rainbow_chaser.rs
@@ -1,3 +1,19 @@
+// led_effects - A collection of LED effects on top of smart-leds.
+// Copyright (C) 2021 Guillaume Cugnet <guillaume@cugnet.eu>
+// Copyright (C) 2021 Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3 of the License.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use core::marker::PhantomData;
 
 use smart_leds::hsv::Hsv;
