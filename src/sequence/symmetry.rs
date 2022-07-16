48489380231c7e3cdcfc61c86fd3f0a411dd0a6c
Jean-Philippe Cugnet <jean-philippe@cugnet.eu>
2022-07-16 21:15:05 +0200

    refactor(sequence)!: add a new size parameter for the sequence in the Symmetry

--- src/sequence/symmetry.rs
+++ src/sequence/symmetry.rs
@@ -16,27 +16,31 @@
 
 use super::Sequence;
 
-/// A sequences that repeats the same sequence twice in symmetry.
-pub struct Symmetry<S: Sequence<N>, const N: usize> {
-    /// The underlying sequencee.
+/// A sequence that repeats the same sequence twice in symmetry.
+pub struct Symmetry<S: Sequence<M>, const N: usize, const M: usize> {
+    /// The underlying sequence.
     sequence: S,
     /// The symmetry.
-    symmetry: [Option<S::Item>; N],
+    symmetry: [Option<S::Item>; M],
     /// The counter.
     counter: usize,
     /// Whether the output is origin or symmetry.
     is_origin: bool,
 }
 
-impl<S: Sequence<N, Item = impl Copy>, const N: usize> Sequence<N>
-    for Symmetry<S, N>
+impl<S: Sequence<M, Item = impl Copy>, const N: usize, const M: usize>
+    Sequence<N> for Symmetry<S, N, M>
 {
     type Config = S::Config;
 
     fn new(config: Self::Config) -> Self {
+        // NOTE: As there is currently no way to perform such assert at compile
+        // time, let’s do it in the constructor at run time.
+        assert!(N == 2 * M || N == 2 * M - 1);
+
         Self {
             sequence: S::new(config),
-            symmetry: [None; N],
+            symmetry: [None; M],
             counter: 0,
             is_origin: true,
         }
@@ -47,8 +51,8 @@ impl<S: Sequence<N, Item = impl Copy>, const N: usize> Sequence<N>
     }
 }
 
-impl<S: Sequence<N, Item = impl Copy>, const N: usize> Iterator
-    for Symmetry<S, N>
+impl<S: Sequence<M, Item = impl Copy>, const N: usize, const M: usize> Iterator
+    for Symmetry<S, N, M>
 {
     type Item = S::Item;
 
@@ -56,7 +60,6 @@ impl<S: Sequence<N, Item = impl Copy>, const N: usize> Iterator
         if self.is_origin {
             if self.counter < N / 2 + N % 2 {
                 let next = self.sequence.next();
-                self.sequence.next();
                 self.symmetry[self.counter] = next;
                 self.counter += 1;
                 next
